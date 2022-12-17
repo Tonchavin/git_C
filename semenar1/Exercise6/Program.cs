a07bbc3800e9986faa62d559a10ae3a32c9a728f
Tonchavin Alexandr <bloodwayn@gmail.com>
2022-12-17 12:47:33 +1000

    add semenar1/Exercise6/Program.cs

--- semenar1/Exercise6/Program.cs
+++ semenar1/Exercise6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+if ((a % 2) == 0)
+{
+    WriteLine($"число {a} является четным");
+}
+else
+{
+    WriteLine($"число {a} является нечетным");
+}
