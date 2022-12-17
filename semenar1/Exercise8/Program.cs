f122c2fc3dacfbf1b1d1f90881120c9b147abca8
Tonchavin Alexandr <bloodwayn@gmail.com>
2022-12-17 13:23:02 +1000

    add semenar1/Exercise8/Program.cs

--- semenar1/Exercise8/Program.cs
+++ semenar1/Exercise8/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+for (int i = 1; i <= a; i++)
+{
+    if (i % 2 == 0)
+    Write($"{i} ");
+}
