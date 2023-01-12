2fb641774367ebcaf7d760ea5edf62ff0b82c6db
Tonchavin Alexandr <bloodwayn@gmail.com>
2023-01-12 12:56:52 +1000

    add semenar3/Exercise23.1

--- semenar3/Exercise23.1/Program.cs
+++ semenar3/Exercise23.1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+string text = "Дан текст.В тексте нужно все пробелы заменить черточками,маленькие буквы к заменить большими К,а большие С маленькими с. ";
+
+// string s = "qwerty";
+//             0123
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '-');
+
+WriteLine(newText);
+WriteLine();
+newText = Replace(text, 'к', 'К');
+WriteLine(newText);
+WriteLine();
+newText = Replace(text, 'С', 'с');
+WriteLine(newText);
